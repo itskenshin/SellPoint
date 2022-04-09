dcdb8c5a863bb376cf17e58c16186db8d70e1842
itskenshin <zomaliense01@gmail.com>
2022-04-08 21:26:02 -0400

    Add project files.

---
+++ SellPoint/forms_screens/Acerca_de_Screen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SellPoint.forms_screens
+{
+    public partial class Acerca_de_Screen : Form
+    {
+        public Acerca_de_Screen()
+        {
+            InitializeComponent();
+        }
+    }
+}
