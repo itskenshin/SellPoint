f45ef075eb4ab14c14ce388f4e4dd28e4a55516c
itskenshin <zomaliense01@gmail.com>
2022-04-09 16:30:05 -0400

    test

--- SellPoint/forms_screens/Acerca_de_Screen.Designer.cs
+++ SellPoint/forms_screens/Acerca_de_Screen.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(256, 245);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(258, 208);
+            this.button1.Size = new System.Drawing.Size(114, 74);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
