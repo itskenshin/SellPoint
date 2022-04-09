14e740197036f7cce5b28e069d9ac0e94b0be79e
itskenshin <zomaliense01@gmail.com>
2022-04-09 01:00:10 -0400

    goddies

--- SellPoint/forms_screens/Login_screen.Designer.cs
+++ SellPoint/forms_screens/Login_screen.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1007, 599);
+            this.ClientSize = new System.Drawing.Size(1081, 623);
             this.ControlBox = false;
             this.Name = "Login_screen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
