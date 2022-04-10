e85e3fdff1e1eb27c8a6e26b33e866f0199e87da
KelvynMarck <kelvinalexandel1234@gmail.com>
2022-04-10 14:21:14 -0400

    actualizada

--- SellPoint/forms_screens/Acerca_de_Screen.cs
+++ SellPoint/forms_screens/Acerca_de_Screen.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace SellPoint.forms_screens
 {
@@ -36,5 +29,5 @@ namespace SellPoint.forms_screens
             this.Close();
         }
     }
-    }
+}
 
