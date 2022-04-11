e41c92b6444cbf513186fd2e595c6546e227083e
itskenshin <zomaliense01@gmail.com>
2022-04-11 13:30:03 -0400

    nuevo form tipoEntidades screen con sus campos

---
+++ SellPoint/forms_screens/tipoEntidadesScreen.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace SellPoint.forms_screens
+{
+    partial class tipoEntidadesScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tipoEntidadesScreen));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.descripFieldtext = new System.Windows.Forms.TextBox();
+            this.labelvali = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.insertBtn = new SellPoint.widgets.RJControls();
+            this.idTippEntidadField = new SellPoint.widgets.textBox();
+            this.deleteBtn = new SellPoint.widgets.RJControls();
+            this.actualiozabtn = new SellPoint.widgets.RJControls();
+            this.rjControls1 = new SellPoint.widgets.RJControls();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-4, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(783, 413);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(344, 72);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(403, 255);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // descripFieldtext
+            // 
+            this.descripFieldtext.Location = new System.Drawing.Point(12, 209);
+            this.descripFieldtext.Multiline = true;
+            this.descripFieldtext.Name = "descripFieldtext";
+            this.descripFieldtext.Size = new System.Drawing.Size(266, 138);
+            this.descripFieldtext.TabIndex = 2;
+            // 
+            // labelvali
+            // 
+            this.labelvali.AutoSize = true;
+            this.labelvali.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelvali.ForeColor = System.Drawing.Color.Red;
+            this.labelvali.Location = new System.Drawing.Point(82, 354);
+            this.labelvali.Name = "labelvali";
+            this.labelvali.Size = new System.Drawing.Size(117, 18);
+            this.labelvali.TabIndex = 4;
+            this.labelvali.Text = "Campos Vacios!";
+            this.labelvali.Visible = false;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsername.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelUsername.Location = new System.Drawing.Point(92, 72);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(110, 20);
+            this.labelUsername.TabIndex = 5;
+            this.labelUsername.Text = "usernameaqui";
+            // 
+            // insertBtn
+            // 
+            this.insertBtn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.insertBtn.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.insertBtn.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.insertBtn.BorderRadius = 20;
+            this.insertBtn.BorderSize = 0;
+            this.insertBtn.FlatAppearance.BorderSize = 0;
+            this.insertBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.insertBtn.ForeColor = System.Drawing.Color.White;
+            this.insertBtn.Location = new System.Drawing.Point(344, 344);
+            this.insertBtn.Name = "insertBtn";
+            this.insertBtn.Size = new System.Drawing.Size(103, 40);
+            this.insertBtn.TabIndex = 6;
+            this.insertBtn.Text = "INSERTAR";
+            this.insertBtn.TextColor = System.Drawing.Color.White;
+            this.insertBtn.UseVisualStyleBackColor = false;
+            this.insertBtn.Click += new System.EventHandler(this.insertBtn_Click);
+            // 
+            // idTippEntidadField
+            // 
+            this.idTippEntidadField.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.idTippEntidadField.BorderColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.idTippEntidadField.BorderFocusColor = System.Drawing.Color.Red;
+            this.idTippEntidadField.BorderSize = 0;
+            this.idTippEntidadField.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idTippEntidadField.ForeColor = System.Drawing.Color.White;
+            this.idTippEntidadField.Location = new System.Drawing.Point(13, 152);
+            this.idTippEntidadField.Margin = new System.Windows.Forms.Padding(4);
+            this.idTippEntidadField.Multiline = false;
+            this.idTippEntidadField.Name = "idTippEntidadField";
+            this.idTippEntidadField.Padding = new System.Windows.Forms.Padding(7);
+            this.idTippEntidadField.PasswordChar = false;
+            this.idTippEntidadField.Size = new System.Drawing.Size(276, 31);
+            this.idTippEntidadField.TabIndex = 3;
+            this.idTippEntidadField.Texts = "";
+            this.idTippEntidadField.UnderlineStyle = false;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.deleteBtn.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.deleteBtn.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.deleteBtn.BorderRadius = 20;
+            this.deleteBtn.BorderSize = 0;
+            this.deleteBtn.FlatAppearance.BorderSize = 0;
+            this.deleteBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteBtn.ForeColor = System.Drawing.Color.White;
+            this.deleteBtn.Location = new System.Drawing.Point(644, 344);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(103, 40);
+            this.deleteBtn.TabIndex = 7;
+            this.deleteBtn.Text = "ELIMINAR";
+            this.deleteBtn.TextColor = System.Drawing.Color.White;
+            this.deleteBtn.UseVisualStyleBackColor = false;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // actualiozabtn
+            // 
+            this.actualiozabtn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.actualiozabtn.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.actualiozabtn.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.actualiozabtn.BorderRadius = 20;
+            this.actualiozabtn.BorderSize = 0;
+            this.actualiozabtn.FlatAppearance.BorderSize = 0;
+            this.actualiozabtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.actualiozabtn.ForeColor = System.Drawing.Color.White;
+            this.actualiozabtn.Location = new System.Drawing.Point(495, 344);
+            this.actualiozabtn.Name = "actualiozabtn";
+            this.actualiozabtn.Size = new System.Drawing.Size(103, 40);
+            this.actualiozabtn.TabIndex = 8;
+            this.actualiozabtn.Text = "ACTUALIZAR";
+            this.actualiozabtn.TextColor = System.Drawing.Color.White;
+            this.actualiozabtn.UseVisualStyleBackColor = false;
+            this.actualiozabtn.Click += new System.EventHandler(this.actualiozabtn_Click);
+            // 
+            // rjControls1
+            // 
+            this.rjControls1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.rjControls1.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.rjControls1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rjControls1.BorderRadius = 10;
+            this.rjControls1.BorderSize = 0;
+            this.rjControls1.FlatAppearance.BorderSize = 0;
+            this.rjControls1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjControls1.ForeColor = System.Drawing.Color.White;
+            this.rjControls1.Location = new System.Drawing.Point(730, 0);
+            this.rjControls1.Name = "rjControls1";
+            this.rjControls1.Size = new System.Drawing.Size(49, 46);
+            this.rjControls1.TabIndex = 9;
+            this.rjControls1.Text = "Salir";
+            this.rjControls1.TextColor = System.Drawing.Color.White;
+            this.rjControls1.UseVisualStyleBackColor = false;
+            this.rjControls1.Click += new System.EventHandler(this.rjControls1_Click);
+            // 
+            // tipoEntidadesScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 401);
+            this.ControlBox = false;
+            this.Controls.Add(this.rjControls1);
+            this.Controls.Add(this.actualiozabtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.insertBtn);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.labelvali);
+            this.Controls.Add(this.idTippEntidadField);
+            this.Controls.Add(this.descripFieldtext);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "tipoEntidadesScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.tipoEntidadesScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox descripFieldtext;
+        private widgets.textBox idTippEntidadField;
+        private System.Windows.Forms.Label labelvali;
+        private System.Windows.Forms.Label labelUsername;
+        private widgets.RJControls insertBtn;
+        private widgets.RJControls deleteBtn;
+        private widgets.RJControls actualiozabtn;
+        private widgets.RJControls rjControls1;
+    }
+}
